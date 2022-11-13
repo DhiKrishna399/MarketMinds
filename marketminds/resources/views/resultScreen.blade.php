72958c98ee4779b69469d36ec15bda3435aaa240
Dhiva Krishna <dhivakrishna@Dhivas-MacBook-Pro-M1.local>
2022-11-12 20:38:16 -0800

    First attempt at routing

---
+++ marketminds/resources/views/resultScreen.blade.php
@@ -0,0 +1,11 @@
+@extends('theme::layouts.main')
+
+<div class="fixed inset-y-0 right-0 w-5/6 items-center place-self-end">
+    <div class="flex flex-col justify-center items-center w-full">
+        <div class="flex rounded-b-xl  bg-gradient-to-r from-[#9BA3EB] via-[#bf9beb] to-[#9bebe3] w-4/5 py-16">
+            <span class="flex w-full text-6xl text-gray-100 font-semibold justify-center">
+                Here's Your Results
+            </span>
+        </div>
+    </div>
+</div>
